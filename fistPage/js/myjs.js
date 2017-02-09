7f2a07b9365b332b92284c292d7774656ae74f48
jihang <jihang@192.168.252.2>
2017-02-09 10:47:20 +0800

    20170209

---
+++ fistPage/js/myjs.js
@@ -0,0 +1,21 @@
+$(function(){
+	clickImg();
+});
+function clickImg(){
+	$("#baoliyun").click(function(){
+		//默认
+		$("#backgroundC").css("background-color","#EA5504")
+	});
+	$("#zulin").click(function(){
+		$("#backgroundC").css("background-color","#438dc9")
+	});
+	$("#ziguan").click(function(){
+		$("#backgroundC").css("background-color","#57d492")
+	});
+	$("#yunpiao").click(function(){
+		$("#backgroundC").css("background-color","#18cfd6")
+	});
+	$("#jiaoyi").click(function(){
+		$("#backgroundC").css("background-color","#e85b09")
+	});
+}
