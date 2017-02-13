b04f7bd045dcaf386090fbafce48a4d1063925d5
jihang <jihang@jihang-PC>
2017-02-13 11:14:25 +0800

    图片跟换

--- fistPage/js/myjs.js
+++ fistPage/js/myjs.js
@@ -7,26 +7,36 @@ function clickImg(){
 		$("#backgroundC").css("background-color","#ff883f");
 		clearClass(this);
 		$(this).children("img").attr("src",'../img/baoliyun1.png');
+		$(this).parent().parent().next().find("h4").html("保理云系统");
+		$(this).parent().parent().next().find("h5").html("平安银行保理云服务平台:是由平安银行为商业保理公司提供的保理业务操作及信息管理服务");
 	});
 	$("#zulin").click(function(){
 		$("#backgroundC").css("background-color","#0066ff");
 		clearClass(this);
 		$(this).children("img").attr("src",'../img/zulin1.png');
+		$(this).parent().parent().next().find("h4").html("内容暂缺");
+		$(this).parent().parent().next().find("h5").html("");
 	});
 	$("#ziguan").click(function(){
 		$("#backgroundC").css("background-color","#89d249");
 		clearClass(this);
 		$(this).children("img").attr("src",'../img/ziguan1.png');
+		$(this).parent().parent().next().find("h4").html("内容暂缺");
+		$(this).parent().parent().next().find("h5").html("");
 	});
 	$("#yunpiao").click(function(){
 		$("#backgroundC").css("background-color","#5f52a0");
 		clearClass(this);
 		$(this).children("img").attr("src",'../img/yunpiao1.png');
+		$(this).parent().parent().next().find("h4").html("内容暂缺");
+		$(this).parent().parent().next().find("h5").html("");
 	});
 	$("#jiaoyi").click(function(){
 		$("#backgroundC").css("background-color","#ea5504");
 		clearClass(this);
 		$(this).children("img").attr("src",'../img/jiaoyi1.png');
+		$(this).parent().parent().next().find("h4").html("内容暂缺");
+		$(this).parent().parent().next().find("h5").html("");
 	});
 }
 function clearClass(a){
